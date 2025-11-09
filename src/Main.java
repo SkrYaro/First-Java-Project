231301b86a24599ff85c14523b40554d26cd440e
Yaroslav <bartsismon333@gmail.com>
2025-11-09 23:07:26 +0100

    GameState:
    create class GameState

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,24 @@ import gameState.GameState;
 class Main {
     public static void main(String[] args){
         GameState game = new GameState();
+        boolean whiteMove = true;
+        while (true) {
+            if (whiteMove) {
+                System.out.println(GameState.getBoard());
 
 
-        System.out.println(GameState.getBoard());
 
+                System.out.printf("white move " + whiteMove);
+
+                whiteMove = !whiteMove;
+            }
+            else {
+
+                System.out.println(GameState.getBoard());
+                System.out.printf("black move " + whiteMove);
+
+                whiteMove = !whiteMove;
+            }
+        }
     }
 }
--- src/gameState/gameObject/Figure.java
+++ src/gameState/gameObject/Figure.java
@@ -2,6 +2,8 @@ package gameState.gameObject;
 
 public abstract class Figure {
 
-    public abstract int[][] move();
+//    public abstract String figureCost();
+
+    public abstract int[] move();
 
 }
