3f47c352edd8beec54dc8f62c79fc8c696e6be6a
Yaroslav <bartsismon333@gmail.com>
2025-11-10 15:35:18 +0100

    issue(gameState):
    Implements: Issue#2

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,53 @@
 import gameState.GameState;
 import gameState.gameObject.Pawn;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args){
         GameState game = new GameState();
         boolean whiteMove = true;
         boolean run = true;
+
+        Scanner chooseMove = new Scanner(System.in);
+
         while (run) {
             if (whiteMove) {
-                System.out.println(GameState.getBoard());
 
-                System.out.println((Pawn.changePos(new int[] {6 , 1} )));
-                System.out.println((Pawn.changePos(new int[] {6 , 3} )));
+                //  Drawind board and helping poss
+                try {
+                    System.out.println(GameState.getHorizontLine());
+                    System.out.println(GameState.getBoard());
 
+                    System.out.println("Write horizontal line (a-h) and vertical (1-8) together");
+                    System.out.println("Example : b2");
+                    int[] poss = GameState.getFigure(chooseMove.nextLine());
+                    System.out.println((Pawn.changePos(poss)));
 
-                System.out.printf("white move " + whiteMove);
-
-                whiteMove = !whiteMove;
+                    System.out.printf("white move on" + poss);
+                    whiteMove = !whiteMove;
+                }catch (Exception e){
+                    System.out.println("Error uncorrect write, try again");
+                }
             }
             else {
+                try {
+                    System.out.println(GameState.getHorizontLine());
+                    System.out.println(GameState.getBoard());
 
-                System.out.println(GameState.getBoard());
-                System.out.printf("black move " + whiteMove);
+                    System.out.println("Write X (1-8) and Y (1-8) together");
+                    System.out.println("Example : 72");
+                    int[] poss = GameState.getFigure(chooseMove.nextLine());
+                    System.out.println((Pawn.changePos(poss)));
 
-                whiteMove = !whiteMove;
+                    System.out.printf("white move on" + poss);
+                    whiteMove = !whiteMove;
+                }catch (Exception e){
+                    System.out.println("Error uncorrect write, try again");
+                }
             }
-            run = false;
+
         }
     }
 }
--- src/gameState/GameState.java
+++ src/gameState/GameState.java
@@ -1,8 +1,15 @@
 package gameState;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class GameState {
+
+    static Scanner chooseMove = new Scanner(System.in);
+
+
+
+
     public static String[][] gameBoard = new String[][]{
             {"bR","bB","bH","bQ","bK","bH","bB","bR"},
             {"bP","bP","bP","bP","bP","bP","bP","bP"},
@@ -14,13 +21,43 @@ public class GameState {
             {"wR","wB","wH","wQ","wK","wH","wB","wR"},
     };
 
+    public static String getHorizontLine(){
+
+        String horizontLine = "  ";
+        for(int i = 0; i<8 ; i++){
+            char letter =  (char)('a' + i );
+            horizontLine +=  "   " + letter;
+        }
+        return horizontLine;
+    };
+
     //visual show where figures
     public static String getBoard() {
         String board = "";
         for (int i = 0; i < GameState.gameBoard.length; i++) {
-            board += Arrays.toString(gameBoard[i]) + '\n';
+            board += (i+1) + ". " + Arrays.toString(gameBoard[i]) + '\n';
         }
         return board;
     };
+
+    public static int[] getFigure(String xy){
+        int x = Character.getNumericValue(xy.charAt(0));
+        int y = Character.getNumericValue(xy.charAt(1));
+
+        if(GameState.gameBoard[x][y] != "  "){
+            int[] poss = new int[2];
+            poss[0] = x;
+            poss[1]= y;
+            return poss;
+        }else if (GameState.gameBoard[x][y].startsWith("b")){
+            System.out.println("You are white ,can't move by black");
+            return GameState.getFigure(chooseMove.nextLine());
+        }
+        else {
+            System.out.println("You choose a void");
+            return GameState.getFigure(chooseMove.nextLine());
+        }
+
+    }
 
 }
